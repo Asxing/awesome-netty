e4bafbff83abfa42e87563fa4105646021403c19
Asxing <AsxingYang@gmail.com>
2021-08-04 17:54:44 +0800

    [ADD] 添加服务端启动代码

---
+++ src/main/java/com/asxing/netty/NettyServer.java
@@ -0,0 +1,63 @@
+package com.asxing.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.util.AttributeKey;
+
+public class NettyServer {
+
+    private static final int BEGIN_PORT = 1000;
+
+    public static void main(String[] args) {
+        NioEventLoopGroup boosGroup = new NioEventLoopGroup();
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        final ServerBootstrap serverBootstrap = new ServerBootstrap();
+        final AttributeKey<Object> clientKey = AttributeKey.newInstance("clientKey");
+        final AttributeKey<Object> serverName = AttributeKey.newInstance("serverName");
+        serverBootstrap
+                .group(boosGroup, workerGroup)
+                .channel(NioServerSocketChannel.class)
+                .attr(serverName, "nettyServer")
+                .childAttr(clientKey, "clientValue")
+                .option(ChannelOption.SO_BACKLOG, 1024)
+                .childOption(ChannelOption.SO_KEEPALIVE, true)
+                .childOption(ChannelOption.TCP_NODELAY, true)
+                .handler(new ChannelInitializer<NioServerSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioServerSocketChannel ch) throws Exception {
+                        System.out.println("服务启动中");
+                        System.out.println("handler attr serverName 对应的值：" + ch.attr(serverName).get());
+                    }
+                })
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel ch) {
+                        System.out.println("新连接建立中");
+                        System.out.println("childHandler attr clientKey 对应的值：" + ch.attr(clientKey).get());
+                    }
+                });
+        bind(serverBootstrap, BEGIN_PORT);
+    }
+
+    /**
+     * 端口绑定
+     *
+     * @param serverBootstrap bootstrap
+     * @param port            端口
+     */
+    private static void bind(final ServerBootstrap serverBootstrap, final int port) {
+        serverBootstrap.bind(port).addListener(future -> {
+            if (future.isSuccess()) {
+                System.out.println("端口[" + port + "]绑定成功!");
+            } else {
+                System.err.println("端口[" + port + "]绑定失败!");
+                bind(serverBootstrap, port + 1);
+            }
+        });
+    }
+}
